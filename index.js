76684a4050f79e72aef11b1028bb599b3b6d88e2
David Aron Vigh <vighdavid@outlook.com>
2023-06-14 09:11:56 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/hello', (req, res) => {
-  res.json({ msg : "Hello world" });
-})
+  res.json({ msg: 'Hello world' });
+});
 
 app.get('/echo/:id', (req, res) => {
-  res.json({ id : req.params.id});
-})
+  res.json({ id: req.params.id });
+});
 
-app.use(express.static('public'))
+app.post('/sum', (req, res) => {
+  const numbers = req.body.numbers;
+  const sum = numbers.reduce((a, b) => a + b, 0);
+  res.json({ sum });
+});
+
+let wordList = [];
+
+app.post('/list', (req, res) => {
+  const text = req.body.text;
+  wordList.push(text);
+  res.json({ list: wordList });
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
